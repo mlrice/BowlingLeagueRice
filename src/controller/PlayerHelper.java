f8b22dcb1fb42d38bb7022415027f028d7558726
JeanTaffae <36548253+JeanTaffae@users.noreply.github.com>
2018-02-23 16:40:35 -0600

    Revert "added player and player helper"
    This reverts commit 37251eb181ece2dc3cf3763f8aaddaaad76e6b73.

--- src/controller/PlayerHelper.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-public class PlayerHelper {
-
-	EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("BowlingLeagueRice");
-}
